c7dc312a8ef03981d475e1ceda66866aad2f6480
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-08-25 10:40:02 -0300

    First Steps in java with Spring Boot

---
+++ src/main/java/com/felipe/rest_with_spring_boot_java/model/Greeting.java
@@ -0,0 +1,5 @@
+package com.felipe.rest_with_spring_boot_java.model;
+
+public record Greeting(Long id, String content) {
+
+}
---
+++ src/test/java/com/felipe/rest_with_spring_boot_java/Startup.java
@@ -0,0 +1,13 @@
+package com.felipe.rest_with_spring_boot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

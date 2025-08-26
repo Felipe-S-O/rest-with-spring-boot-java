a0d5408ca3277846b855308310ee08e65b3c4e16
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-08-26 10:02:51 -0300

    feat: refatorando o codigo separando os conceitos

--- src/main/java/com/felipe/rest_with_spring_boot_java/controllers/MathController.java
+++ src/main/java/com/felipe/rest_with_spring_boot_java/controllers/MathController.java
@@ -5,55 +5,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipe.rest_with_spring_boot_java.exception.UnsupportedMathOperationException;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.felipe.rest_with_spring_boot_java.math.SimpleMath;
+import com.felipe.rest_with_spring_boot_java.request.converters.NumberConverter;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
 
+    private SimpleMath math = new SimpleMath();
+
     // http://localhost:8080/math/sum/3/5
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
             throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Defina um valor numérico válido.");
         }
-        return covertToDouble(numberOne) + covertToDouble(numberTwo);
-    }
-
-    private Double covertToDouble(String strNumber) throws IllegalAccessException {
-        if (strNumber == null || strNumber.isEmpty())
-            throw new UnsupportedMathOperationException("Defina um valor numérico válido.");
-        String number = strNumber.replaceAll(",", ".");// Moeda Americana x Brasileira
-        return Double.parseDouble(number);
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null || strNumber.isEmpty())
-            return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+        return math.sum(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
     }
 
     // http://localhost:8080/math/subtraction/3/5
     @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
     public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
             throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Defina um valor numérico válido.");
         }
-        return covertToDouble(numberOne) - covertToDouble(numberTwo);
+        return math.subtraction(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
     }
 
     // http://localhost:8080/math/multiplication/3/5
     @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
     public Double multiplication(@PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Defina um valor numérico válido.");
 
-        return covertToDouble(numberOne) * covertToDouble(numberTwo);
+        return math.multiplication(NumberConverter.covertToDouble(numberOne),
+                NumberConverter.covertToDouble(numberTwo));
 
     }
 
@@ -61,10 +50,10 @@ public class MathController {
     @RequestMapping("division/{numberOne}/{numberTwo}")
     public Double division(@PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Defina um valor numérico válido.");
 
-        return covertToDouble(numberOne) / covertToDouble(numberTwo);
+        return math.division(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
 
     }
 
@@ -72,19 +61,19 @@ public class MathController {
     @RequestMapping("/mean/{numberOne}/{numberTwo}")
     public Double maen(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo)
             throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Defina um valor numérico válido.");
 
-        return (covertToDouble(numberOne) + covertToDouble(numberTwo)) / 2;
+        return math.maen(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
     }
 
     // http://localhost:8080/math/squareRoot/81
     @RequestMapping("/squareroot/{number}")
     public Double squareRoot(@PathVariable("number") String number) throws Exception {
-        if (!isNumeric(number))
+        if (!NumberConverter.isNumeric(number))
             throw new UnsupportedMathOperationException("Defina um valor numérico válido.");
 
-        return Math.sqrt(covertToDouble(number));
+        return math.squareRoot(NumberConverter.covertToDouble(number));
 
     }
 }

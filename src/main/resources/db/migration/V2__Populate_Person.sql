9ac658ebf33e40b4826c848d04e69cacba9d604c
Felipe Silva <63815922+Felipe-S-O@users.noreply.github.com>
2025-09-09 23:36:39 -0300

    feat: Migratins flyway

---
+++ src/main/resources/db/migration/V2__Populate_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'João Pessoa - PB', 'Luciene', 'Male', 'Silva'),
+	(2, 'João Pessoa - PB', 'Felipe', 'Male', 'Silva'),
+	(3, 'João Pessoa - PB', 'Fabiana', 'Male', 'Silva'),
+	(4, 'João Pessoa - PB', 'Rosilene', 'Male', 'Silva');
+
